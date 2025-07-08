f4c897928e4071432ff3b10cf01c6bdd31a43dc5
Devmike01 <michaelgbenga91@gmail.com>
2025-07-08 23:50:26 +0100

    Added DSTestHelper

---
+++ dsa/src/test/kotlin/dev/gbenga/dsa/DSTestHelper.kt
@@ -0,0 +1,31 @@
+package dev.gbenga.dsa
+
+import dev.gbenga.dsa.collections.Queue
+import dev.gbenga.dsa.collections.Stack
+import kotlin.test.assertTrue
+
+inline fun <reified T> areTheSame(actual: Stack<T>, expected: Stack<T>){
+    var areTheSame = actual.size() == expected.size()
+    if (areTheSame){
+        while (!actual.isEmpty() && !expected.isEmpty()){
+            if (actual.pop() != expected.pop()){
+                areTheSame = false
+                break
+            }
+        }
+    }
+    assertTrue(areTheSame)
+}
+
+//inline fun <reified T> areTheSame(actual: Queue<T>, expected: Queue<T>){
+//    var areTheSame = actual.size() != expected.size()
+//    if (areTheSame){
+//        while (!actual.isEmpty() && !expected.isEmpty()){
+//            if (actual.pop() != expected.pop()){
+//                areTheSame = false
+//                break
+//            }
+//        }
+//    }
+//    assertTrue(areTheSame)
+//}
