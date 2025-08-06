8f294fae30566e2c45ac35b05bb83ec907f4a195
Devmike01 <michaelgbenga91@gmail.com>
2025-08-07 00:10:05 +0100

    Added search logic

--- app/src/main/java/dev/gbenga/dsagithub/features/home/HomeViewModel.kt
+++ app/src/main/java/dev/gbenga/dsagithub/features/home/HomeViewModel.kt
@@ -7,6 +7,7 @@ import dev.gbenga.dsa.collections.Queue
 import dev.gbenga.dsa.collections.QueueImpl
 import dev.gbenga.dsa.collections.list.LinkedList
 import dev.gbenga.dsa.collections.list.LinkedListImpl
+import dev.gbenga.dsa.collections.list.linkedListOf
 import dev.gbenga.dsagithub.base.AppViewModel
 import dev.gbenga.dsagithub.base.MenuIcon
 import dev.gbenga.dsagithub.base.MenuId
@@ -14,12 +15,15 @@ import dev.gbenga.dsagithub.base.MenuItem
 import dev.gbenga.dsagithub.base.UiState
 import dev.gbenga.dsagithub.data.database.Favourite
 import dev.gbenga.dsagithub.features.home.data.User
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -41,13 +45,16 @@ class HomeViewModel(private val favouriteRepository: FavouriteRepository,
     private val _userList : LinkedList<User> = LinkedListImpl()
     private var _endQueue: Queue<Int> = QueueImpl<Int>(ENDLESS_SCROLL_SIZE)
 
+    private var previousSearch : MenuId? = null
+
     companion object{
         const val ENDLESS_SCROLL_SIZE = 2
         const val REFRESH_SCREEN = "HomeViewModel.REFRESH_SCREEN"
     }
 
     init {
-        println("REFRESH_SCREEN: ${savedState.get<Boolean>(REFRESH_SCREEN)}")
+        loadMenus()
+       loadGithubUsers()
     }
 
     fun loadMoreGithubUsers(){
@@ -117,7 +124,7 @@ class HomeViewModel(private val favouriteRepository: FavouriteRepository,
 
 
     fun loadMenus(){
-        _menus.update {
+        _menus.update { //
             LinkedListImpl<MenuItem>().apply {
                 prepend(MenuItem(icon = MenuIcon.REVERSE,
                     id = MenuId.REVERSE))
@@ -125,7 +132,7 @@ class HomeViewModel(private val favouriteRepository: FavouriteRepository,
                     id = MenuId.SWAP))
                 prepend(MenuItem(icon = MenuIcon.SORT,
                     id = MenuId.SORT))
-                prepend(MenuItem(icon = MenuIcon.SORT,
+                prepend(MenuItem(icon = MenuIcon.SEARCH,
                     id = MenuId.SEARCH))
             }
         }
@@ -162,17 +169,42 @@ class HomeViewModel(private val favouriteRepository: FavouriteRepository,
     }
 
     fun setOnMenuClick(id: MenuId){
-        when(id){
-            MenuId.REVERSE -> {
-                Log.d("MenuItem", "--> MenuItem: $id")
-                reverseUsers()
-            }
-            MenuId.SORT -> {}
-            MenuId.SWAP -> {}
-            MenuId.SEARCH -> {
+        viewModelScope.launch {
+            when(id){
+                MenuId.REVERSE -> {
+                    Log.d("MenuItem", "--> MenuItem: $id")
+                    reverseUsers()
+                }
+                MenuId.SORT -> {}
+                MenuId.SWAP -> {}
+                MenuId.SEARCH -> {
 
+                    previousSearch?.let {
+                        loadMenus()
+                        previousSearch = null
+                    } ?: _menus.update { menus ->
+                        previousSearch = MenuId.SEARCH
+                        var curr = menus.peekHeadNode()
+                        val updatedList = linkedListOf<MenuItem>()
+                        while (curr != null){
+                            updatedList.prepend(curr.data.copy(
+                                hide = curr.data.id != MenuId.SEARCH
+                            ))
+                            curr = curr.next
+                        }
+                        updatedList
+                    }
+                    _action.emit(
+                        HomeUiEvent(menuAction = MenuId.SEARCH)
+                    )
+                }
+
+                MenuId.RESET -> {
+                    loadMenus()
+                }
+                else -> {}
             }
-            else -> {}
         }
     }
+
 }
